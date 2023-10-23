f957890210e59d4fc38de6b9a1cc6f1cda7a8f4b
Pedro <pedrohvidals@gmail.com>
2023-10-22 22:20:36 -0300

    [feat] - criando nova DTO para receber todos os dados de um paciente

--- src/main/java/com/pedro/demo/patients/PatientDTO.java
+++ src/main/java/com/pedro/demo/patients/PatientDTO.java
@@ -1,20 +1,9 @@
 package com.pedro.demo.patients;
 
-import com.pedro.demo.endereco.DadosEndereco;
 import com.pedro.demo.endereco.Endereco;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import org.springframework.format.annotation.NumberFormat;
 
-public record PatientDTO (
-        @NotBlank String nome,
-        @NotBlank @Email String email,
-        @NotBlank String telefone,
-        @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
-        @NotNull @Valid DadosEndereco endereco,
-        @NotNull int quarto
-){
+public record PatientDTO (Long id, String nome, String email, String telefone, String crm, Endereco endereco, int quarto){
+    public PatientDTO(Patient patient){
+        this(patient.getId(), patient.getNome(), patient.getEmail(), patient.getTelefone(), patient.getCrm(), patient.getEndereco(), patient.getQuarto());
+    }
 }
