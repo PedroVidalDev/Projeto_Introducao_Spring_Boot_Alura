4355456937594424638b7880d6cd1b13c40941f7
Pedro <pedrohvidals@gmail.com>
2023-10-23 23:05:51 -0300

    [feat] - adicionando tratamento para erro 404

--- src/main/java/com/pedro/demo/infra/TratamentoDeErros.java
+++ src/main/java/com/pedro/demo/infra/TratamentoDeErros.java
@@ -1,7 +1,15 @@
 package com.pedro.demo.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TratamentoDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
 }
