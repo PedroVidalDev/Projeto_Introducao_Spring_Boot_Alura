89b95a1b58e4e07e587b1eff1c198eec494a6c9a
Pedro <pedrohvidals@gmail.com>
2023-10-24 00:05:20 -0300

    [feat] - criando service para fazer a autenticacao, junto de funcao de validacao de usuario que sera executada quando cliente tentar fazer login, fazendo uma busca no banco de dados se esse usuario realmente existe

--- src/main/java/com/pedro/demo/domain/users/UserRepository.java
+++ src/main/java/com/pedro/demo/domain/users/UserRepository.java
@@ -1,6 +1,8 @@
 package com.pedro.demo.domain.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
 }
