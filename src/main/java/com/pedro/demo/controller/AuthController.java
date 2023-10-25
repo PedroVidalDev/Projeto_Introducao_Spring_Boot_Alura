e9dc1e81be62df2be804e0ef35af192ebff6a789
Pedro <pedrohvidals@gmail.com>
2023-10-25 19:09:26 -0300

    [feat] - recolhendo token no controller e enviando para cliente

--- src/main/java/com/pedro/demo/controller/AuthController.java
+++ src/main/java/com/pedro/demo/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.pedro.demo.controller;
 
+import com.pedro.demo.domain.users.User;
 import com.pedro.demo.domain.users.UserDataLoginDTO;
+import com.pedro.demo.infra.security.TokenJwtDataDTO;
+import com.pedro.demo.infra.security.TokenService;
 import jakarta.validation.Valid;
+import org.hibernate.event.spi.ResolveNaturalIdEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,11 +22,17 @@ public class AuthController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid UserDataLoginDTO userDataLoginDTO){
+
         var token = new UsernamePasswordAuthenticationToken(userDataLoginDTO.login(), userDataLoginDTO.senha());
         var auth = manager.authenticate(token);
 
-        return ResponseEntity.ok(auth.toString());
+        var tokenJWT = tokenService.gerarToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new TokenJwtDataDTO(tokenJWT));
     }
 }
