8fa93ad7cbd6b70ca543d173606a44b8a47826c5
Pedro <pedrohvidals@gmail.com>
2023-10-31 18:45:51 -0300

    [feat] - adicionando funcionalidade de consulta de medico randomica

--- src/main/java/com/pedro/demo/domain/medic/MedicRepository.java
+++ src/main/java/com/pedro/demo/domain/medic/MedicRepository.java
@@ -3,7 +3,24 @@ package com.pedro.demo.domain.medic;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MedicRepository extends JpaRepository<Medic, Long> {
     Page<Medic> findAllByLativoTrue(Pageable paginacao);
+
+    @Query("""
+            select m from Medic m
+            where m.lativo = true
+            and m.especialidade = :especialidade
+            and m.id not in(
+                select s.medic.id from Schedule s
+                where
+                s.date = :date
+            )
+            order by rand()
+            limit 1
+            """)
+    Medic choiceRandomMedic(Especialidade especialidade, LocalDateTime date);
 }
