7421d8762349a11ab967303c4fba3ed5f3d7efec
Pedro <pedrohvidals@gmail.com>
2023-10-31 22:12:18 -0300

    [feat] - adicionando funcoes de busca personalizada no repository de consulta

--- src/main/java/com/pedro/demo/domain/schedule/ScheduleRepository.java
+++ src/main/java/com/pedro/demo/domain/schedule/ScheduleRepository.java
@@ -2,5 +2,10 @@ package com.pedro.demo.domain.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    boolean existsByMedicIdAndDate(Long idMedic, LocalDateTime date);
+
+    boolean existsByPatientIdAndDateBetween(Long idPatient, LocalDateTime firstHour, LocalDateTime lastHour);
 }
