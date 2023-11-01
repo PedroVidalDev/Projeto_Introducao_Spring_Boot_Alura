96d3e590bf0086bdc4d8d6c30ceb912b166655df
Pedro <pedrohvidals@gmail.com>
2023-10-31 22:10:09 -0300

    [feat] - puxando as validacoes para a service

--- src/main/java/com/pedro/demo/domain/schedule/ScheduleService.java
+++ src/main/java/com/pedro/demo/domain/schedule/ScheduleService.java
@@ -4,9 +4,12 @@ import com.pedro.demo.domain.medic.Medic;
 import com.pedro.demo.domain.medic.MedicRepository;
 import com.pedro.demo.domain.patients.Patient;
 import com.pedro.demo.domain.patients.PatientRepository;
+import com.pedro.demo.domain.schedule.validations.ValidatorSchedule;
 import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ScheduleService {
@@ -20,7 +23,10 @@ public class ScheduleService {
     @Autowired
     private PatientRepository patientRepository;
 
-    public void makeSchedule(ScheduleDataInput data){
+    @Autowired
+    private List<ValidatorSchedule> validators;
+
+    public ScheduleDataOutputDTO makeSchedule(ScheduleDataInputDTO data){
         System.out.println(data);
         if(!patientRepository.existsById(data.idPatient())){
            throw new ValidationExceptionSchedule("ID do paciente informado nao existe.");
@@ -30,14 +36,22 @@ public class ScheduleService {
             throw new ValidationExceptionSchedule("ID do medico informado nao existe.");
         }
 
+        validators.forEach(v -> v.validar(data));
+
         Medic medic = choiceMedic(data);
+        if(medic == null){
+            throw new ValidationExceptionSchedule("Nao existe medico disponivel");
+        }
+
         Patient patient = patientRepository.findById(data.idPatient()).get();
 
         Schedule schedule = new Schedule(null, medic, patient, null, data.date());
         repository.save(schedule);
+
+        return new ScheduleDataOutputDTO(schedule);
     }
 
-    private Medic choiceMedic(ScheduleDataInput data){
+    private Medic choiceMedic(ScheduleDataInputDTO data){
         if(data.idMedic() != null){
             return medicRepository.getReferenceById(data.idMedic());
         }
@@ -47,7 +61,7 @@ public class ScheduleService {
         return medicRepository.choiceRandomMedic(data.especialidade(), data.date());
     }
 
-    public void cancelSchedule(ScheduleDeleteDataInput data){
+    public void cancelSchedule(ScheduleDeleteDataInputDTO data){
         if(!repository.existsById(data.idSchedule())){
             throw new ValidationException("Consulta nao existe.");
         }
